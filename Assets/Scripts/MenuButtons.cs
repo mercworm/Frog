7730bbea2de6b15574f9d45c2d708322dc8b78c6
SAE\rjansson <rebecka-miranda@hotmail.com>
2018-09-03 09:55:14 +1000

    Added Unity Project Made placehlder main menu + script to move between scenes scenes not hooked up yet

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour {
+
+    public GameObject creditsScreen;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Frog");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        //Showing the credits when leftshift is held down
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            creditsScreen.SetActive(true);
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            creditsScreen.SetActive(false);
+        }
+	}
+}
