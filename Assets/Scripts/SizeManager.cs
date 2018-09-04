30c5e373097cf2c2146fd92420ce68fc855cb2e9
SAE\rjansson <rebecka-miranda@hotmail.com>
2018-09-04 11:34:59 +1000

    Tried the fly catching (works) a rigidbody popped out of the hinge joint for some reason. fixed fixed up the size manager

--- Assets/Scripts/SizeManager.cs
+++ Assets/Scripts/SizeManager.cs
@@ -7,29 +7,40 @@ public class SizeManager : MonoBehaviour {
     //The different sizes the frogs can be.
     public Sprite[] playerSizes;
     //The amount of flies they have eaten.
-    public int flyCount;
+    public int flyCount = 0;
+    public int currentSize = 0;
 
     Rigidbody2D rb2d;
+    SpriteRenderer frogBody;
 
     public float massIncrease;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        frogBody = GetComponentInParent<SpriteRenderer>();
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Fly"))
         {
-            ChangeSize();
-            Destroy(collision.gameObject); // this could be changed to do something else
+            Debug.Log("Touched fly");
+            //ChangeSize();
+            //Destroy(collision.gameObject); // this could be changed to do something else
         }
     }
 
     public void ChangeSize ()
     {
+        //change the size of the frog
+        //this should be configured depending on the flycount, so we can change how many flies a frog needs to eat to grow.
+        //this will hold for now though.
+        currentSize++;
+        frogBody.sprite = playerSizes[currentSize];
+
         flyCount++;
+
         rb2d.mass += massIncrease;
         EventManager.TriggerEvent("IncreaseJumpForce");
 
