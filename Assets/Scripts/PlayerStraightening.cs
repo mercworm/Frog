3109a9d13e266593051c6b128fbfc4dfb129c117
SAE\rjansson <rebecka-miranda@hotmail.com>
2018-09-06 15:27:10 +1000

    steve helped fix the falling over bug that makes one frog unable to move

---
+++ Assets/Scripts/PlayerStraightening.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStraightening : MonoBehaviour {
+
+    public float selfRigtingTol = 0.5f;
+    public float torque = 1;
+
+    Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update ()
+    {
+        if (Vector2.Dot(transform.up, Vector2.up) < selfRigtingTol)
+        {
+            rb2d.AddTorque(torque * transform.up.x);
+        }
+	}
+}
